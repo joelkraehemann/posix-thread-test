909d8cbfcf844494e4c358d4984d403a070f8183
Joël Krähemann <jkraehemann@gmail.com>
2017-01-04 00:30:50 +0100

    added atomic operations and implemented test_thread_get_toplevel() and test_thread_set_sync_all()

--- functional_pthread_cond_test.c
+++ functional_pthread_cond_test.c
@@ -1,5 +1,9 @@
 #include "functional_pthread_cond_test.h"
 
+TestThread* test_thread_alloc(TestThread *test_thread);
+
+TestThread* test_thread_get_toplevel(TestThread *test_thread);
+
 void test_thread_set_sync_all(TestThread *test_thread, int tic);
 int test_thread_is_tree_ready(TestThread *test_thread);
 
@@ -11,7 +15,7 @@ TestThread* test_thread_soundcard_poll_alloc();
 TestThread* test_thread_ui_processing_alloc();
 
 void* test_thread_main_loop_run(void *ptr);
-void* test_thread_midi_pool_run(void *ptr);
+void* test_thread_midi_poll_run(void *ptr);
 void* test_thread_audio_processing_run(void *ptr);
 void* test_thread_channel_processing_run(void *ptr);
 void* test_thread_soundcard_poll_run(void *ptr);
@@ -19,10 +23,126 @@ void* test_thread_ui_processing_run(void *ptr);
 
 pthread_mutex_t *test_thread_tree_mutex = NULL;
 
+TestThread*
+test_thread_alloc(TestThread *test_thread)
+{
+}
+
+TestThread*
+test_thread_get_toplevel(TestThread *test_thread)
+{
+  if(test_thread == NULL){
+    return(NULL);
+  }
+  
+  while(test_atomic_pointer_get(&(thread->parent)) != NULL){
+    thread = test_atomic_pointer_get(&(thread->parent));
+  }
+
+  return(thread);
+}
+
 void
 test_thread_set_sync_all(TestThread *test_thread, int tic)
 {
-  //TODO:JK: implement me
+  TestThread *main_loop;
+
+  auto void test_thread_set_sync_all_recursive(TestThread *thread, int tic);
+  TestThread *child;
+
+  guint next_tic;
+  guint flags;
+  gboolean waiting;
+
+  waiting = FALSE;
+
+  pthread_mutex_lock(thread->sync_mutex);
+
+  flags = test_atomic_uint_get(&(thread->flags));
+
+  /* get next tic and check waiting */
+  switch(tic){
+  case 0:
+    {
+      if((TEST_THREAD_WAIT_0 & flags) != 0){
+	next_tic = 1;
+	waiting = TRUE;
+      }
+    }
+    break;
+  case 1:
+    {
+      if((TEST_THREAD_WAIT_1 & flags) != 0){
+	next_tic = 2;
+	waiting = TRUE;
+      }
+    }
+    break;
+  case 2:
+    {
+      if((TEST_THREAD_WAIT_2 & flags) != 0){
+	next_tic = 0;
+	waiting = TRUE;
+      }
+    }
+    break;
+  }
+
+  /* unset wait flag */
+  switch(tic){
+  case 0:
+    {
+      if((TEST_THREAD_WAIT_0 & flags) != 0){
+	test_atomic_uint_and(&(thread->flags),
+			     (~TEST_THREAD_WAIT_0));
+      }
+    }
+    break;
+  case 1:
+    {
+      if((TEST_THREAD_WAIT_1 & flags) != 0){
+	test_atomic_uint_and(&(thread->flags),
+			     (~TEST_THREAD_WAIT_1));
+      }
+    }
+    break;
+  case 2:
+    {
+      if((TEST_THREAD_WAIT_2 & flags) != 0){
+	test_atomic_uint_and(&(thread->flags),
+			     (~TEST_THREAD_WAIT_2));
+      }
+    }
+    break;
+  }
+
+  /* signal waiting thread */
+  test_atomic_uint_and(&(thread->flags),
+		       (~TEST_THREAD_WAITING));
+
+  if((TEST_THREAD_WAITING & (test_atomic_uint_get(&(thread->flags)))) != 0){
+    pthread_cond_signal(thread->sync_cond);
+  }    
+
+  thread->current_tic = next_tic;
+
+  /*  */
+  pthread_mutex_unlock(thread->sync_mutex);
+
+  /* descend the tree */
+  child = test_atomic_pointer_get(&(thread->children));
+
+  while(child != NULL){
+    test_thread_set_sync_all_recursive(child, tic);
+      
+    child = test_atomic_pointer_get(&(child->next));
+  }
+}
+
+/* entry point */
+main_loop = test_thread_get_toplevel(thread);
+
+test_thread_set_sync_all_recursive(main_loop, tic);
 }
 
 int
--- functional_pthread_cond_test.h
+++ functional_pthread_cond_test.h
@@ -12,18 +12,21 @@ typedef enum{
   TEST_THREAD_WAIT_1                  = 1 <<  4,
   TEST_THREAD_WAIT_2                  = 1 <<  5,
   TEST_THREAD_READY                   = 1 <<  6,
-  TEST_THREAD_INTERMEDIATE_PRE_SYNC   = 1 <<  7,
-  TEST_THREAD_INTERMEDIATE_POST_SYNC  = 1 <<  8,
+  TEST_THREAD_WAITING                 = 1 <<  7,
+  TEST_THREAD_INTERMEDIATE_PRE_SYNC   = 1 <<  8,
+  TEST_THREAD_INTERMEDIATE_POST_SYNC  = 1 <<  9,
 }TestThreadFlags;
 
 struct _TestThread
 {
   volatile unsigned int flags;
 
-  char *buffer;
-
+  int current_tic;
+  
   pthread_mutex_t *sync_mutex;
   pthread_cond_t *sync_cond;
+
+  char *buffer;
 
   pthread_t *thread;
 
@@ -34,3 +37,45 @@ struct _TestThread
 
   volatile TestThread *children;
 };
+
+#define test_atomic_uint_get(atomic)					\
+  (G_GNUC_EXTENSION ({							\
+      G_STATIC_ASSERT (sizeof *(atomic) == sizeof (unsinged int));	\
+      (void) (0 ? *(atomic) ^ *(atomic) : 0);				\
+      __sync_synchronize ();						\
+      (unsinged int) *(atomic);						\
+    }))
+#define test_atomic_uint_set(atomic, newval)				\
+  (G_GNUC_EXTENSION ({							\
+      G_STATIC_ASSERT (sizeof *(atomic) == sizeof (unsinged int));	\
+      (void) (0 ? *(atomic) ^ (newval) : 0);				\
+      *(atomic) = (newval);						\
+      __sync_synchronize ();						\
+    }))
+
+#define test_atomic_pointer_get(atomic)				\
+  (G_GNUC_EXTENSION ({						\
+      G_STATIC_ASSERT (sizeof *(atomic) == sizeof (void *));	\
+      __sync_synchronize ();					\
+      (void *) *(atomic);					\
+    }))
+#define test_atomic_pointer_set(atomic, newval)			\
+  (G_GNUC_EXTENSION ({						\
+      G_STATIC_ASSERT (sizeof *(atomic) == sizeof (void *));	\
+      (void) (0 ? (void *) *(atomic) : 0);			\
+      *(atomic) = (__typeof__ (*(atomic))) (gsize) (newval);	\
+      __sync_synchronize ();					\
+    }))
+
+#define test_atomic_uint_and(atomic, val)				\
+  (G_GNUC_EXTENSION ({							\
+      G_STATIC_ASSERT (sizeof *(atomic) == sizeof (unsinged int));	\
+      (void) (0 ? *(atomic) ^ (val) : 0);				\
+      (guint) __sync_fetch_and_and ((atomic), (val));			\
+    }))
+#define test_atomic_uint_or(atomic, val)				\
+  (G_GNUC_EXTENSION ({							\
+      G_STATIC_ASSERT (sizeof *(atomic) == sizeof (unsinged int));	\
+      (void) (0 ? *(atomic) ^ (val) : 0);				\
+      (guint) __sync_fetch_and_or ((atomic), (val));			\
+    }))
